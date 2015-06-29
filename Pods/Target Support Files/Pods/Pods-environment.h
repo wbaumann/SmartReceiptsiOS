d9dc957fc6845a8530522f645b67ab86315c2bc0
Jaanus Siim <jaanus@jaanussiim.com>
2015-06-29 20:51:51 +0300

    Add RMStore

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -102,6 +102,30 @@
 #define COCOAPODS_VERSION_MINOR_MRProgress_Stopable 8
 #define COCOAPODS_VERSION_PATCH_MRProgress_Stopable 2
 
+// OpenSSL
+#define COCOAPODS_POD_AVAILABLE_OpenSSL
+#define COCOAPODS_VERSION_MAJOR_OpenSSL 1
+#define COCOAPODS_VERSION_MINOR_OpenSSL 0
+#define COCOAPODS_VERSION_PATCH_OpenSSL 203
+
+// RMStore/AppReceiptVerificator
+#define COCOAPODS_POD_AVAILABLE_RMStore_AppReceiptVerificator
+#define COCOAPODS_VERSION_MAJOR_RMStore_AppReceiptVerificator 0
+#define COCOAPODS_VERSION_MINOR_RMStore_AppReceiptVerificator 7
+#define COCOAPODS_VERSION_PATCH_RMStore_AppReceiptVerificator 1
+
+// RMStore/Core
+#define COCOAPODS_POD_AVAILABLE_RMStore_Core
+#define COCOAPODS_VERSION_MAJOR_RMStore_Core 0
+#define COCOAPODS_VERSION_MINOR_RMStore_Core 7
+#define COCOAPODS_VERSION_PATCH_RMStore_Core 1
+
+// RMStore/KeychainPersistence
+#define COCOAPODS_POD_AVAILABLE_RMStore_KeychainPersistence
+#define COCOAPODS_VERSION_MAJOR_RMStore_KeychainPersistence 0
+#define COCOAPODS_VERSION_MINOR_RMStore_KeychainPersistence 7
+#define COCOAPODS_VERSION_PATCH_RMStore_KeychainPersistence 1
+
 // UIAlertView-Blocks
 #define COCOAPODS_POD_AVAILABLE_UIAlertView_Blocks
 #define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 1
