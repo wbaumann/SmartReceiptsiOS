c377b7e00f8d48829f890275ffa5f2afeaf8d184
Jaanus Siim <jaanus@jaanussiim.com>
2015-07-12 22:44:32 +0300

    Adjust columns width to table width

--- SmartReceipts/PDF/PDFReportTable.m
+++ SmartReceipts/PDF/PDFReportTable.m
@@ -8,6 +8,7 @@
 
 #import "PDFReportTable.h"
 #import "TableHeaderRow.h"
+#import "Constants.h"
 
 @interface PDFReportTable ()
 
@@ -38,9 +39,13 @@
 
 - (void)buildTable {
     NSMutableArray *columnsWidth = [NSMutableArray arrayWithCapacity:self.columns.count];
-    for (NSInteger column = 0; column < self.columns.count; column++) {
+    NSMutableArray *titleWidth = [NSMutableArray arrayWithCapacity:self.columns.count];
+    for (NSUInteger column = 0; column < self.columns.count; column++) {
         [columnsWidth addObject:@([self maxWidthOfColumn:column])];
+        [titleWidth addObject:@(ceilf([self.headerRowPrototype widthForValue:self.columns[column]]))];
     }
+
+    columnsWidth = [self divideRemainingWidth:columnsWidth titleWidth:titleWidth];
 
     CGFloat yOffset = [self appendRow:self.columns columnWidths:columnsWidth usingPrototype:self.headerRowPrototype yOffset:0];
     for (NSInteger row = 0; row < self.rows.count; row++) {
@@ -52,6 +57,73 @@
         }
         yOffset = [self appendRow:self.rows[row] columnWidths:columnsWidth usingPrototype:prototype yOffset:yOffset];
     }
+}
+
+- (NSMutableArray *)divideRemainingWidth:(NSMutableArray *)columns titleWidth:(NSArray *)titleWidth {
+    NSNumber *width = [columns valueForKeyPath:@"@sum.self"];
+    CGFloat columnsWidth = [width floatValue];
+    if (columnsWidth <= CGRectGetWidth(self.frame)) {
+        columns = [self divideRemainingEqually:columns columnsWidth:columnsWidth];
+    } else {
+        columns = [self fitColumnsWidth:columns titleWidth:titleWidth];
+    }
+
+    return columns;
+}
+
+- (NSMutableArray *)fitColumnsWidth:(NSMutableArray *)columns titleWidth:(NSArray *)titleWidths {
+    NSMutableIndexSet *indexesToModify = [NSMutableIndexSet indexSet];
+    NSMutableIndexSet *indexesNotToModify = [NSMutableIndexSet indexSet];
+    for (NSUInteger index = 0; index < columns.count; index++) {
+        if ([self canSqueezeColumn:self.columns[index]]) {
+            [indexesToModify addIndex:index];
+        } else {
+            [indexesNotToModify addIndex:index];
+        }
+    }
+
+    NSNumber *totalWidthOfNotModified = [[columns objectsAtIndexes:indexesNotToModify] valueForKeyPath:@"@sum.self"];
+    NSNumber *totalWidthOfModified = [[columns objectsAtIndexes:indexesToModify] valueForKeyPath:@"@sum.self"];
+    NSNumber *modifiedTitleWidths = [[titleWidths objectsAtIndexes:indexesToModify] valueForKeyPath:@"@sum.self"];
+    CGFloat spaceToDivide = CGRectGetWidth(self.frame) - totalWidthOfNotModified.floatValue - modifiedTitleWidths.floatValue;
+
+    NSMutableArray *modifiedColumns = [NSMutableArray arrayWithCapacity:columns.count];
+    for (NSUInteger index = 0; index < columns.count; index++) {
+        if ([indexesNotToModify containsIndex:index]) {
+            [modifiedColumns addObject:columns[index]];
+            continue;
+        }
+
+        NSNumber *columnWidth = columns[index];
+        CGFloat percent = columnWidth.floatValue / totalWidthOfModified.floatValue;
+        NSNumber *titleWidth = titleWidths[index];
+        [modifiedColumns addObject:@(titleWidth.floatValue + floorf(spaceToDivide * percent))];
+    }
+
+    return modifiedColumns;
+}
+
+- (BOOL)canSqueezeColumn:(NSString *)title {
+    return [NSLocalizedString(@"receipt.column.comment", nil) isEqualToString:title]
+            || [NSLocalizedString(@"receipt.column.name", nil) isEqualToString:title]
+            || [NSLocalizedString(@"receipt.column.report.comment", nil) isEqualToString:title];
+}
+
+- (NSMutableArray *)divideRemainingEqually:(NSMutableArray *)columns columnsWidth:(CGFloat)columnsWidth {
+    CGFloat remainder = CGRectGetWidth(self.frame) - columnsWidth;
+    CGFloat toAdd = ceilf(remainder / (CGFloat)columns.count);
+    NSMutableArray *added = [NSMutableArray arrayWithCapacity:columns.count];
+    for (NSInteger index = 0; index < columns.count; index++) {
+        NSNumber *number = columns[index];
+        if (toAdd > remainder) {
+            toAdd = remainder;
+        }
+
+        [added addObject:@(number.floatValue + toAdd)];
+        remainder -= toAdd;
+    }
+
+    return added;
 }
 
 - (CGFloat)appendRow:(NSArray *)array columnWidths:(NSArray *)widths usingPrototype:(TableContentRow *)prototype yOffset:(CGFloat)yOffset {
@@ -104,7 +176,7 @@
         maxWidth = MAX(maxWidth, [self.rowOnePrototype widthForValue:value]);
     }
 
-    return maxWidth;
+    return ceilf(maxWidth);
 }
 
 @end
