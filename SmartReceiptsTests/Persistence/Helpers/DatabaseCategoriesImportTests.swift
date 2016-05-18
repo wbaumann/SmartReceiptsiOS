863e178dcaf06bfa6adae86358bb9af7d3a7f89c
Jaanus Siim <jaanus@jaanussiim.com>
2016-05-18 09:43:40 +0300

    Add Swift to tests

---
+++ SmartReceiptsTests/Persistence/Helpers/DatabaseCategoriesImportTests.swift
@@ -0,0 +1,33 @@
+//
+//  DatabaseCategoriesImportTests.swift
+//  SmartReceipts
+//
+//  Created by Jaanus Siim on 18/05/16.
+//  Copyright © 2016 Will Baumann. All rights reserved.
+//
+
+import XCTest
+
+class DatabaseCategoriesImportTests: XCTestCase {
+    private var pathToImport: String!
+    private var databasePath: String!
+    
+    override func setUp() {
+        super.setUp()
+
+    }
+    
+    override func tearDown() {
+
+        super.tearDown()
+    }
+    
+    func testCategoriesImportWithOverwrite() {
+
+    }
+    
+    func testCategoriesImportWithoutOverwrite() {
+
+    }
+    
+}
