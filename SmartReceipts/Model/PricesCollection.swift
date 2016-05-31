0071e8e61cc548ed136a57273726b1ede0e334e1
Jaanus Siim <jaanus@jaanussiim.com>
2016-05-31 10:13:10 +0300

    Swiftify PricesCollection

---
+++ SmartReceipts/Model/PricesCollection.swift
@@ -0,0 +1,59 @@
+//
+//  PricesCollection.swift
+//  SmartReceipts
+//
+//  Created by Jaanus Siim on 31/05/16.
+//  Copyright © 2016 Will Baumann. All rights reserved.
+//
+
+import Foundation
+
+class PricesCollection: Price {
+    private var totals = [String: NSDecimalNumber]()
+    
+    func addPrice(price: Price?) {
+        guard let  price = price else {
+            return
+        }
+        
+        add(price.amount, currency: price.currency.code())
+    }
+    
+    func subtractPrice(price: Price?) {
+        guard let  price = price else {
+            return
+        }
+        
+        add(price.amount.decimalNumberByMultiplyingBy(.minusOne()), currency: price.currency.code())
+    }
+    
+    func hasValue() -> Bool {
+        let values = totals.values
+        
+        for amount in values {
+            if amount.compare(NSDecimalNumber.zero()) != .OrderedSame {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    override func currencyFormattedPrice() -> String {
+        var formats = [String]()
+        let codes = totals.keys.sort()
+        for code in codes {
+            let amount = totals[code]!
+            let price = Price(amount: amount, currencyCode: code)
+            formats.append(price.currencyFormattedPrice())
+        }
+        
+        return formats.joinWithSeparator("; ")
+    }
+    
+    private func add(amount: NSDecimalNumber, currency: String) {
+        let total = totals[currency] ?? NSDecimalNumber.zero()
+        let newTotal = total.decimalNumberByAdding(amount)
+        totals[currency] = newTotal
+    }
+}
---
+++ SmartReceiptsTests/Model/PricesCollectionTests.swift
@@ -0,0 +1,52 @@
+//
+//  PricesCollectionTests.swift
+//  SmartReceipts
+//
+//  Created by Jaanus Siim on 31/05/16.
+//  Copyright © 2016 Will Baumann. All rights reserved.
+//
+
+import XCTest
+@testable import SmartReceipts
+
+class PricesCollectionTests: XCTestCase {
+    private var collection: PricesCollection!
+    
+    override func setUp() {
+        super.setUp()
+
+        collection = PricesCollection()
+    }
+    
+    func testSingleCurrencyAdd() {
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "10"), currencyCode: "USD"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "20"), currencyCode: "USD"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "30"), currencyCode: "USD"))
+        
+        let expected = Price(amount: NSDecimalNumber(string: "60"), currencyCode: "USD")
+        XCTAssertEqual(expected.currencyFormattedPrice(), collection.currencyFormattedPrice())
+    }
+    
+    func testSingleCurrencyAddRemove() {
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "10"), currencyCode: "USD"))
+        collection.subtractPrice(Price(amount: NSDecimalNumber(string: "5"), currencyCode: "USD"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "20"), currencyCode: "USD"))
+        collection.subtractPrice(Price(amount: NSDecimalNumber(string: "10"), currencyCode: "USD"))
+        
+        let expected = Price(amount: NSDecimalNumber(string: "15"), currencyCode: "USD")
+        XCTAssertEqual(expected.currencyFormattedPrice(), collection.currencyFormattedPrice())
+    }
+    
+    func testMultiCurrencyAdd() {
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "10"), currencyCode: "USD"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "20"), currencyCode: "EUR"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "30"), currencyCode: "USD"))
+        collection.addPrice(Price(amount: NSDecimalNumber(string: "40"), currencyCode: "EUR"))
+        
+        let expectedEUR = Price(amount: NSDecimalNumber(string: "60"), currencyCode: "EUR")
+        let expectedUSD = Price(amount: NSDecimalNumber(string: "40"), currencyCode: "USD")
+        
+        let expectedResult = [expectedEUR.currencyFormattedPrice(), expectedUSD.currencyFormattedPrice()].joinWithSeparator("; ")
+        XCTAssertEqual(expectedResult, collection.currencyFormattedPrice())
+    }
+}
