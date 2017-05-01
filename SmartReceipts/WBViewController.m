2eb2852386fca2488d0aacd1e0a2397df92bd697
Victor <victor.dombrovskiy@gmail.com>
2017-05-01 13:29:52 +0300

    - extra code removed

--- SmartReceipts/WBViewController.m
+++ SmartReceipts/WBViewController.m
@@ -28,15 +28,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-    
     [WBCustomization customizeOnViewDidLoad:self];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
