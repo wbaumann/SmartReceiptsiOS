59fdc81d3cf1d50238366f7a57a56cbcc6907285
Evsenev <evsenev.developer@gmail.com>
2017-09-03 01:08:04 +0300

    Added FeatureFlags

---
+++ SmartReceipts/Utils/FeatureFlags.swift
@@ -0,0 +1,23 @@
+//
+//  FeatureManager.swift
+//  SmartReceipts
+//
+//  Created by Bogdan Evsenev on 03/09/2017.
+//  Copyright © 2017 Will Baumann. All rights reserved.
+//
+
+import Foundation
+
+class FeatureFlags {
+    static let loginSupport = Feature(true)
+    static let ocrSupport   = Feature(true)
+}
+
+class Feature {
+    private(set) var isEnabled: Bool
+    
+    init(_ enabled: Bool) {
+        isEnabled = enabled
+    }
+}
+
