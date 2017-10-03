86e8a765ea34e53a50bca2563d1b05999266dc43
Evsenev <evsenev.developer@gmail.com>
2017-10-03 21:59:22 +0300

    Purchase Service connceted with our API

--- SmartReceipts/Services/PurchaseService.swift
+++ SmartReceipts/Services/PurchaseService.swift
@@ -9,6 +9,7 @@
 import RMStore
 import RxSwift
 import StoreKit
+import SwiftyJSON
 
 let PRODUCT_OCR_10 = "ios_ocr_purchase_10"
 let PRODUCT_OCR_50 = "ios_ocr_purchase_50"
@@ -16,7 +17,7 @@ let PRODUCT_PLUS = "ios_plus_sku_2"
 
 class PurchaseService {
     private var plusSubsribtionProduct: SKProduct?
-    private let bag = DisposeBag()
+    fileprivate let bag = DisposeBag()
     
     func requestProducts() -> Observable<SKProduct> {
         let ids: Set = [PRODUCT_PLUS, PRODUCT_OCR_10, PRODUCT_OCR_50]
@@ -35,14 +36,19 @@ class PurchaseService {
     }
     
     func purchaseSubscription() -> Observable<SKPaymentTransaction> {
-        AnalyticsManager.sharedManager.record(event: Event.Navigation.SmartReceiptsPlusOverflow)
-        return RMStore.default().addPayment(product: PRODUCT_PLUS)
-            .do(onNext: { transaction in
-                PurchaseService.analyticsPurchaseSuccess(productID: PRODUCT_PLUS)
+        return purchase(prodcutID: PRODUCT_PLUS)
+            .do(onNext: { _ in
                 NotificationCenter.default.post(name: NSNotification.Name.SmartReceiptsAdsRemoved, object: nil)
-            }, onError: { [weak self] error in
-                PurchaseService.analyticsPurchaseFailed(productID: PRODUCT_PLUS)
-                self?.handleError(error)
+            }, onError: handleError(_:))
+    }
+    
+    func purchase(prodcutID: String) -> Observable<SKPaymentTransaction> {
+        return RMStore.default().addPayment(product: prodcutID)
+            .do(onNext: { [weak self] transaction in
+                PurchaseService.analyticsPurchaseSuccess(productID: prodcutID)
+                self?.sendConfirm(transaction: transaction)
+            }, onError: { _ in
+                PurchaseService.analyticsPurchaseFailed(productID: prodcutID)
             })
     }
     
@@ -57,17 +63,44 @@ class PurchaseService {
             AnalyticsManager.sharedManager.record(event: errorEvent)
         }
     }
-    
-    private static func analyticsPurchaseSuccess(productID: String) {
+}
+
+
+// MARK: Purchase and API
+extension PurchaseService {
+    func sendConfirm(transaction: SKPaymentTransaction) {
+        if AuthService.shared.isLoggedIn {
+            guard let receiptURL = Bundle.main.appStoreReceiptURL else { return }
+            guard let receipt = try? Data(contentsOf: receiptURL) else { return }
+            let receiptString = receipt.base64EncodedString()
+            Logger.debug("RECEIPT: \(receiptString)")
+            let params = ["encoded_receipt": receiptString,
+                          "pay_service" : "Apple Store",
+                          "goal" : "Recognition"]
+            
+            APIAdapter.jsonBody(.post, endpoint("mobile_app_purchases"), parameters: params)
+                .subscribe(onNext: { response in
+                    let jsonRespose = JSON(response)
+                    Logger.debug(jsonRespose.description)
+                }, onError: { error in
+                    Logger.error(error.localizedDescription)
+                }).disposed(by: bag)
+        }
+    }
+}
+
+
+// MARK: Purchase Analytics
+extension PurchaseService {
+    fileprivate static func analyticsPurchaseSuccess(productID: String) {
         let anEvent =  Event.Purchases.PurchaseSuccess
         anEvent.dataPoints.append(DataPoint(name: "sku", value: productID))
         AnalyticsManager.sharedManager.record(event: anEvent)
     }
     
-    private static func analyticsPurchaseFailed(productID: String) {
+    fileprivate static func analyticsPurchaseFailed(productID: String) {
         let anEvent =  Event.Purchases.PurchaseFailed
         anEvent.dataPoints.append(DataPoint(name: "sku", value: productID))
         AnalyticsManager.sharedManager.record(event: anEvent)
     }
-    
 }
