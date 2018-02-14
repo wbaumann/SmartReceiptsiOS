49a2c4381201bb7f19f5f267da711c3bd40bfe0e
Evsenev <evsenev.developer@gmail.com>
2018-02-14 20:57:47 +0300

    Added Backup Module

---
+++ SmartReceipts/Modules/Backup/BackupDisplayData.swift
@@ -0,0 +1,14 @@
+//
+//  BackupDisplayData.swift
+//  SmartReceipts
+//
+//  Created by Bogdan Evsenev on 14/02/2018.
+//Copyright © 2018 Will Baumann. All rights reserved.
+//
+
+import Foundation
+import Viperit
+
+class BackupDisplayData: DisplayData {
+
+}
---
+++ SmartReceipts/Modules/Backup/BackupPresenter.swift
@@ -0,0 +1,27 @@
+//
+//  BackupPresenter.swift
+//  SmartReceipts
+//
+//  Created by Bogdan Evsenev on 14/02/2018.
+//Copyright © 2018 Will Baumann. All rights reserved.
+//
+
+import Foundation
+import Viperit
+
+class BackupPresenter: Presenter {
+}
+
+
+// MARK: - VIPER COMPONENTS API (Auto-generated code)
+private extension BackupPresenter {
+    var view: BackupViewInterface {
+        return _view as! BackupViewInterface
+    }
+    var interactor: BackupInteractor {
+        return _interactor as! BackupInteractor
+    }
+    var router: BackupRouter {
+        return _router as! BackupRouter
+    }
+}
