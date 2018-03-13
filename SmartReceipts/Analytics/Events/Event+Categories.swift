8dbceeb659714c81062149fc48f3db45a6c7bea5
Evsenev <evsenev.developer@gmail.com>
2018-03-13 20:53:15 +0300

    Added metrics to Generate (nothing to generate for CSV)

--- SmartReceipts/Analytics/Events/Event+Categories.swift
+++ SmartReceipts/Analytics/Events/Event+Categories.swift
@@ -112,6 +112,7 @@ public extension Event {
         static let ImagesPdfReport = Event(category: Category.Generate, name: "ImagesPdfReport")
         static let CsvReport = Event(category: Category.Generate, name: "CsvReport")
         static let StampedZipReport = Event(category: Category.Generate, name: "StampedZipReport")
+        static let NothingToGenerateCSV = Event(category: Category.Generate, name: "EmptyReportsAndDisabledDistancesCSV")
     }
     
     struct Ratings {
