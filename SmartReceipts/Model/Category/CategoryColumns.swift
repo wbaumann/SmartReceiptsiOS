d8fdb44dc3e2b2960db51bcf4dfa05c8d1eec0cf
Evsenev <evsenev.developer@gmail.com>
2018-05-06 08:03:11 +0300

    [Fix] Category Optional value

--- SmartReceipts/Model/Category/CategoryColumns.swift
+++ SmartReceipts/Model/Category/CategoryColumns.swift
@@ -25,7 +25,7 @@ class CategoryColumn: Column {
     
     func valueFrom(receipts: [WBReceipt]) -> String {
         guard let firstReceipt = receipts.first else { return "" }
-        return "\(firstReceipt.category.name) [\(receipts.count)]"
+        return "\(firstReceipt.category.name ?? "") [\(receipts.count)]"
     }
 }
 
