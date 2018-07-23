ddedb2bf031dd601829a3fad88b560ec3df3eba2
Evsenev <evsenev.developer@gmail.com>
2018-07-24 00:14:36 +0300

    Added AppNotificationsCenter - notifications with Rx support

---
+++ SmartReceipts/Rx/AppNotificationCenter.swift
@@ -0,0 +1,31 @@
+//
+//  AppNotificationCenter.swift
+//  SmartReceipts
+//
+//  Created by Bogdan Evsenev on 23/07/2018.
+//  Copyright © 2018 Will Baumann. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+@objc class AppNotificationCenter: NSObject {
+    private static let wifiRelay = BehaviorRelay(value: WBPreferences.autobackupWifiOnly())
+    private static let syncProviderRelay = BehaviorRelay(value: SyncProvider.current)
+    
+    // MARK: - Subscribe
+    
+    class var preferencesWiFiOnly: Observable<Bool> { return wifiRelay.asObservable() }
+    class var syncProvider: Observable<SyncProvider> { return syncProviderRelay.asObservable() }
+    
+    // MARK: - Post
+    
+    @objc class func postWifiPreferencesUpdate(_ value: Bool) {
+        AppNotificationCenter.wifiRelay.accept(value)
+    }
+    
+    class func postSyncProviderChanged(_ value: SyncProvider) {
+        AppNotificationCenter.syncProviderRelay.accept(value)
+    }
+}
