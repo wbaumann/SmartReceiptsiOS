24dff19e76142830690c1f1f7f575a7f60b6584a
Evsenev <evsenev.developer@gmail.com>
2018-08-04 00:29:25 +0300

    Added overflow menu to Backup Item: Restore, Delete, Download to ZIP, [Debug] Download

--- SmartReceipts/Modules/Backup/Item/BackupItemView.swift
+++ SmartReceipts/Modules/Backup/Item/BackupItemView.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import RxSwift
 
 class BackupItemView: UIView {
     @IBOutlet private weak var deviceName: UILabel!
     @IBOutlet private weak var serviceName: UILabel!
     @IBOutlet private weak var syncDate: UILabel!
+    @IBOutlet private weak var menu: UIButton!
     
     func setup(backup: RemoteBackupMetadata) {
         deviceName.text = backup.syncDeviceName
         serviceName.text = SyncProvider.current.localizedTitle()
         syncDate.text = WBDateFormatter().formattedDate(backup.lastModifiedDate, in: .current)
+    }
+    
+    func onMenuTap() -> Observable<Void> {
+        return menu.rx.tap.asObservable()
     }
 
 }
