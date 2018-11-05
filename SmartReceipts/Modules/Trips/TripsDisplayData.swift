2a19180e15371ea5feffeb92a44b51e9920c3f9e
Evsenev <evsenev.developer@gmail.com>
2018-11-05 21:08:18 +0300

    Updated our Smart Receipts toolbar title based on if this is a paid user or not

--- SmartReceipts/Modules/Trips/TripsDisplayData.swift
+++ SmartReceipts/Modules/Trips/TripsDisplayData.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import Viperit
-import MKDropdownMenu
 import RxSwift
 
 fileprivate typealias MenuItem = (title: String, subject: PublishSubject<Void>)
@@ -19,22 +18,10 @@ final class TripsDisplayData: DisplayData {
     private let backupSubject = PublishSubject<Void>()
     private let userGuideSubject = PublishSubject<Void>()
     
-    private(set) var menuDisplayData: TripsMenuDisplayData!
-    
     var settingsTap: Observable<Void> { return settingsSubject.asObservable() }
     var autoScansTap: Observable<Void> { return autoScansSubject.asObservable() }
     var backupTap: Observable<Void> { return backupSubject.asObservable() }
     var userGuideTap: Observable<Void> { return userGuideSubject.asObservable() }
-    
-    required init() {
-        let items: [MenuItem]  = [
-            (LocalizedString("menu_main_settings"), settingsSubject),
-            (LocalizedString("ocr_configuration_title"), autoScansSubject),
-            (LocalizedString("backups"), backupSubject),
-            (LocalizedString("menu_main_usage_guide"), backupSubject)
-        ]
-        menuDisplayData = TripsMenuDisplayData(items: items)
-    }
     
     func makeActions() -> [UIAlertAction] {
         let settingsAction = UIAlertAction(title: LocalizedString("menu_main_settings"),
@@ -52,33 +39,4 @@ final class TripsDisplayData: DisplayData {
         return [settingsAction, ocrSettingsAction, backupAction, userGuideAction]
     }
     
-}
-
-// MENU
-class TripsMenuDisplayData: NSObject, MKDropdownMenuDelegate, MKDropdownMenuDataSource  {
-    private var items = [MenuItem]()
-    
-    fileprivate init(items: [MenuItem]) {
-        self.items = items
-    }
-    
-    func dropdownMenu(_ dropdownMenu: MKDropdownMenu, numberOfRowsInComponent component: Int) -> Int {
-        return items.count
-    }
-    
-    func numberOfComponents(in dropdownMenu: MKDropdownMenu) -> Int {
-        return 1
-    }
-    
-    func dropdownMenu(_ dropdownMenu: MKDropdownMenu, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        return NSAttributedString(string: items[row].title, attributes: [
-            .font: UIFont.systemFont(ofSize: 16, weight: .medium),
-            .foregroundColor: AppTheme.primaryColor
-        ])
-    }
-    
-    func dropdownMenu(_ dropdownMenu: MKDropdownMenu, didSelectRow row: Int, inComponent component: Int) {
-        items[row].subject.onNext(())
-        dropdownMenu.closeAllComponents(animated: true)
-    }
 }
