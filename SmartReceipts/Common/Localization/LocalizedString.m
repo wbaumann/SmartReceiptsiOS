605a2029fb7ba81250737cff6cc60ceae06b6923
Will Baumann <wbaumann@github.com>
2018-11-15 16:16:37 -0500

    Added a fallback language support to English
    By default, iOS will return the key value rather than a defined translation. To simplify things for our users, I've added support for a default fallback mechansim to the english bundle.

--- SmartReceipts/Common/Localization/LocalizedString.m
+++ SmartReceipts/Common/Localization/LocalizedString.m
@@ -11,9 +11,26 @@
 @implementation LocalizedString
 
 + (NSString*)from:(NSString *)key comment:(NSString *)comment {
-    NSString* result = NSLocalizedString(key, comment);
+    // By default, attempt to load from our Shared set of strings
+    NSString* result = NSLocalizedStringFromTable(key, @"SharedLocalizable", comment);
     if ([result isEqualToString:key]) {
-        result = NSLocalizedStringFromTable(key, @"SharedLocalizable", comment);
+        // If we failed to find this string in our SharedLocalizable.strings file, check Localizable.strings one
+        result = NSLocalizedString(key, comment);
+    }
+    if ([result isEqualToString:key]) {
+        // If we cannot find it in either, fall back to English
+        NSString* path = [[NSBundle mainBundle] pathForResource:@"en" ofType:@"lproj"];
+        if (path) {
+            NSBundle* enBundle = [NSBundle bundleWithPath:path];
+            if (enBundle) {
+                // Check the English Localizable.strings file
+                result = [enBundle localizedStringForKey:key value:@"" table:nil];
+                if ([result isEqualToString:key]) {
+                    // If we failed to find this string in our SharedLocalizable.strings file, check Localizable.strings one
+                    result = [enBundle localizedStringForKey:key value:@"" table:@"SharedLocalizable"];
+                }
+            }
+        }
     }
     return result;
 }
