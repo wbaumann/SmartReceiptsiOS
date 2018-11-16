4fb41bc288be70a489586f964b652d56045700a2
Evsenev <evsenev.developer@gmail.com>
2018-11-16 23:31:19 +0300

    Added alert if user denied camera permissions

--- SmartReceipts/Common/UI/ImagePicker.swift
+++ SmartReceipts/Common/UI/ImagePicker.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxSwift
+import AVKit
 
 private let COMPRESSION_RATIO: CGFloat = 0.95
 
@@ -63,6 +64,24 @@ class ImagePicker: NSObject {
     @discardableResult
     private func presentPicker(on viewController: UIViewController, source: UIImagePickerController.SourceType) -> Single<UIImage> {
         let picker = UIImagePickerController()
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) == .denied {
+            let title = LocalizedString("generic_error_alert_title")
+            let message = LocalizedString("camera_permission_alert_message")
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let cancel = UIAlertAction(title: LocalizedString("DIALOG_CANCEL"), style: .cancel, handler: nil)
+            let settings = UIAlertAction(title: LocalizedString("camera_permission_alert_button"), style: .default) { _ in
+                guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+            
+            alert.addAction(cancel)
+            alert.addAction(settings)
+            
+            viewController.present(alert, animated: true, completion: nil)
+            return .never()
+        }
+        
         picker.delegate = self
         picker.sourceType = source
         
