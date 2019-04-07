f2264d85ffd5cfd0c82272682c4ad16ad5838d36
Evsenev <evsenev.developer@gmail.com>
2019-04-08 00:10:34 +0300

    [Fix] issue in which the iOS microphone prints double results

--- SmartReceipts/Common/Utils/EurekaWhitespaceWorkaround.swift
+++ SmartReceipts/Common/Utils/EurekaWhitespaceWorkaround.swift
@@ -11,7 +11,18 @@
 import Foundation
 import Eureka
 
+private let REPLACING_CHAR = "\u{00a0}"
+private let DICTATION_ID = "dictation"
+
 final class EurekaWhitespaceWorkaround {
+    private var textField: UITextField?
+    private var isDictation = false
+    
+    init() {
+        let name = UITextInputMode.currentInputModeDidChangeNotification
+        let sel = #selector(keyboardModeChanged)
+        NotificationCenter.default.addObserver(self, selector: sel, name: name, object: nil)
+    }
     
     static func configureTextCells() {
         let workaround = EurekaWhitespaceWorkaround()
@@ -22,17 +33,32 @@ final class EurekaWhitespaceWorkaround {
     }
     
     func configureWhiteSpaceWorkaround(forTextCell cell: TextCell) {
+        textField = cell.textField
+        
         cell.textField.addTarget(self, action: #selector(EurekaWhitespaceWorkaround.replaceNormalSpacesWithNonBreakingSpaces(textField:)), for: .editingChanged)
         cell.textField.addTarget(self, action: #selector(EurekaWhitespaceWorkaround.replaceNonBreakingSpacesWithNormalSpaces(textField:)), for: .editingDidEnd)
     }
     
     @objc private func replaceNormalSpacesWithNonBreakingSpaces(textField: UITextField) {
+        if isDictation { return }
+        
         let cursor = textField.selectedTextRange
-        textField.text = textField.text?.replacingOccurrences(of: " ", with: "\u{00a0}")
+        textField.text = textField.text?.replacingOccurrences(of: " ", with: REPLACING_CHAR)
         textField.selectedTextRange = cursor
     }
     
     @objc private func replaceNonBreakingSpacesWithNormalSpaces(textField: UITextField) {
-        textField.text = textField.text?.replacingOccurrences(of: "\u{00a0}", with: " ")
+        textField.text = textField.text?.replacingOccurrences(of: REPLACING_CHAR, with: " ")
+    }
+    
+    @objc func keyboardModeChanged(notification: Notification) {
+        let idSelector = #selector(getter: UILayoutGuide.identifier)
+        guard textField?.textInputMode?.responds(to: idSelector) == true,
+            let id = textField?.textInputMode?.perform(idSelector)?.takeUnretainedValue() as? String
+        else {
+            return
+        }
+        
+        isDictation = id.contains(DICTATION_ID)
     }
 }
