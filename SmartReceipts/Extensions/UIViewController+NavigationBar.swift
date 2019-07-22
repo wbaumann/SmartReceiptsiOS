150766e709b0c10cb29180a9afb1111adeb208ea
Bogdan Evsenev <evsenev.developer@gmail.com>
2019-07-23 00:49:06 +0300

    [Fix] NavigationBar on iPad (iOS 10.3)

--- SmartReceipts/Extensions/UIViewController+NavigationBar.swift
+++ SmartReceipts/Extensions/UIViewController+NavigationBar.swift
@@ -14,16 +14,22 @@ extension UIViewController {
         titleLabel.textAlignment = .center
         titleLabel.textColor = color
         titleLabel.text = title
+        titleLabel.sizeToFit()
         
         let subtitleLabel = UILabel(frame: .zero)
         subtitleLabel.font = .systemFont(ofSize: 14)
         subtitleLabel.textAlignment = .center
         subtitleLabel.textColor = color
         subtitleLabel.text = subtitle
+        subtitleLabel.sizeToFit()
         
         let stackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
         stackView.alignment = .center
         stackView.axis = .vertical
+        
+        let width = max(titleLabel.bounds.width, subtitleLabel.bounds.width)
+        let height = titleLabel.bounds.height + subtitleLabel.bounds.height
+        stackView.frame.size = .init(width: width, height: height)
         
         navigationItem.titleView = stackView
     }
