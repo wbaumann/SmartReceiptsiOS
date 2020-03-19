8d3533080f1e9296a7573056b34425a7282d347d
Bogdan Evsenev <evsenev.developer@gmail.com>
2020-03-19 20:19:57 +0300

    Added tooltip with Plus users and backup recovery

--- SmartReceipts/Services/Local/TooltipService.swift
+++ SmartReceipts/Services/Local/TooltipService.swift
@@ -12,25 +12,13 @@ import RxSwift
 class TooltipService {
     private let bag = DisposeBag()
     
-    fileprivate let MOVE_TO_GENERATE_DISMISSED = "move.to.generate.dismissed"
-    fileprivate let REPORT_GENERATED = "move.to.generate.dismissed"
-    fileprivate let CONFIGURE_OCR = "ocr.configure.dismissed"
-    fileprivate let PRIVACY_OPENED = "privacy.opened"
-    fileprivate let PRIVACY_DISSMISED = "privacy.dissmissed"
-    fileprivate let REMINDER_DATE = "reminder.date"
-    fileprivate let REMINDER_INSERTS = "reminder.inserts"
-    fileprivate let REMINDER_HAS_BACKUP = "reminder.has.backup"
-    
-    fileprivate let MIN_INSERTS_COUNT = 15
-    fileprivate let MIN_DAYS_COUNT = 10
-    
     fileprivate var database: Database!
     
     private init() {
         database = Database.sharedInstance()
         NotificationCenter.default.addObserver(self, selector: #selector(didInsert(_:)), name: .DatabaseDidInsertModel, object: nil)
-        if UserDefaults.standard.double(forKey: REMINDER_DATE) == 0 {
-            UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: REMINDER_DATE)
+        if UserDefaults.standard.double(forKey: Keys.REMINDER_DATE) == 0 {
+            UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: Keys.REMINDER_DATE)
         }
         
         AppNotificationCenter.didSyncBackup.subscribe(onNext: { [weak self] in
@@ -49,7 +37,15 @@ class TooltipService {
     func tooltipText(for module: AppModules) -> String? {
         
         switch module {
-        case .trips: return tooltipPrivacy()
+        case .trips:
+            let privacyTooltip = tooltipPrivacy()
+            if privacyTooltip != nil {
+                return privacyTooltip
+            } else {
+                return backupPlusReminder()
+            }
+        case .receipts:
+            return backupPlusReminder()
         default: break
         }
         
@@ -64,10 +60,17 @@ class TooltipService {
         return nil
     }
     
+    func backupPlusReminder() -> String? {
+        if PurchaseService.hasValidSubscriptionValue && !marked(key: Keys.REMINDER_PLUS_BACKUP) {
+            return LocalizedString("tooltip_automatic_backups_recovery_hint")
+        }
+        return nil
+    }
+    
     func tooltipBackupReminder() -> String? {
         if needRemindByInserts() && needRemindByDays() {
-            if UserDefaults.standard.bool(forKey: REMINDER_HAS_BACKUP) {
-                let timeInterval = UserDefaults.standard.double(forKey: REMINDER_DATE)
+            if UserDefaults.standard.bool(forKey: Keys.REMINDER_HAS_BACKUP) {
+                let timeInterval = UserDefaults.standard.double(forKey: Keys.REMINDER_DATE)
                 let backupDate = Date(timeIntervalSince1970: timeInterval)
                 let days = Date().daysDifference(date: backupDate)
                 return String(format: LocalizedString("tooltip_backup_info_message"), days)
@@ -79,13 +82,14 @@ class TooltipService {
     }
     
     func tooltipPrivacy() -> String? {
-        let inserts = UserDefaults.standard.integer(forKey: REMINDER_INSERTS)
-        return Locale.current.isEurope || inserts > 0 && !privacyTooltipUsed() ? LocalizedString("tooltip_review_privacy") : nil
+        let inserts = UserDefaults.standard.integer(forKey: Keys.REMINDER_INSERTS)
+        return (Locale.current
+            .isEurope || inserts > 0) && !privacyTooltipUsed() ? LocalizedString("tooltip_review_privacy") : nil
     }
     
     @objc private func didInsert(_ notification: Notification) {
-        let inserts = UserDefaults.standard.integer(forKey: REMINDER_INSERTS) + 1
-        UserDefaults.standard.set(inserts, forKey: REMINDER_INSERTS)
+        let inserts = UserDefaults.standard.integer(forKey: Keys.REMINDER_INSERTS) + 1
+        UserDefaults.standard.set(inserts, forKey: Keys.REMINDER_INSERTS)
     }
 }
 
@@ -94,58 +98,62 @@ extension TooltipService {
     
     // Mark
     func markMoveToGenerateDismiss() {
-        mark(key: MOVE_TO_GENERATE_DISMISSED)
+        mark(key: Keys.MOVE_TO_GENERATE_DISMISSED)
     }
     
     func markReportGenerated() {
-        mark(key: REPORT_GENERATED)
+        mark(key: Keys.REPORT_GENERATED)
     }
     
     func markConfigureOCRDismissed() {
-        mark(key: CONFIGURE_OCR)
+        mark(key: Keys.CONFIGURE_OCR)
     }
     
     func markPrivacyOpened() {
-        mark(key: PRIVACY_OPENED)
+        mark(key: Keys.PRIVACY_OPENED)
     }
     
     func markPrivacyDismissed() {
-        mark(key: PRIVACY_DISSMISED)
+        mark(key: Keys.PRIVACY_DISSMISED)
     }
     
     func markBackupReminderDismissed() {
-        UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: REMINDER_DATE)
+        UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: Keys.REMINDER_DATE)
+    }
+    
+    func markBackupPlusDismissed() {
+        mark(key: Keys.REMINDER_PLUS_BACKUP)
     }
     
     func markBackup() {
-        UserDefaults.standard.set(true, forKey: REMINDER_HAS_BACKUP)
-        UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: REMINDER_DATE)
+        UserDefaults.standard.set(true, forKey: Keys.REMINDER_HAS_BACKUP)
+        UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: Keys.REMINDER_DATE)
     }
     
     // Get
     func moveToGenerateTrigger(for trip: WBTrip) -> Bool {
         let hasReceipts = database.allReceipts(for: trip).count > 0
-        return !marked(key: MOVE_TO_GENERATE_DISMISSED) && !marked(key: REPORT_GENERATED) && hasReceipts
+        return !marked(key: Keys.MOVE_TO_GENERATE_DISMISSED) && !marked(key: Keys.REPORT_GENERATED) && hasReceipts
     }
     
     func configureOCRDismissed() -> Bool {
-        return marked(key: CONFIGURE_OCR)
+        return marked(key: Keys.CONFIGURE_OCR)
     }
     
     func privacyTooltipUsed() -> Bool {
-        return marked(key: PRIVACY_OPENED) || marked(key: PRIVACY_DISSMISED)
+        return marked(key: Keys.PRIVACY_OPENED) || marked(key: Keys.PRIVACY_DISSMISED)
     }
     
     fileprivate func needRemindByDays() -> Bool {
-        let timeInterval = UserDefaults.standard.double(forKey: REMINDER_DATE)
+        let timeInterval = UserDefaults.standard.double(forKey: Keys.REMINDER_DATE)
         if timeInterval != 0 { return false }
         let backupDate = Date(timeIntervalSince1970: timeInterval)
-        return Date().daysDifference(date: backupDate) >= MIN_DAYS_COUNT
+        return Date().daysDifference(date: backupDate) >= Constants.MIN_DAYS_COUNT
     }
     
     fileprivate func needRemindByInserts() -> Bool {
-        let inserts = UserDefaults.standard.integer(forKey: REMINDER_INSERTS)
-        return inserts >= MIN_INSERTS_COUNT
+        let inserts = UserDefaults.standard.integer(forKey: Keys.REMINDER_INSERTS)
+        return inserts >= Constants.MIN_INSERTS_COUNT
     }
     
     // Private
@@ -155,5 +163,24 @@ extension TooltipService {
     
     private func marked(key: String) -> Bool {
         return UserDefaults.standard.bool(forKey: key)
+    }
+}
+
+private extension TooltipService {
+    enum Keys {
+        static let MOVE_TO_GENERATE_DISMISSED = "move.to.generate.dismissed"
+        static let REPORT_GENERATED = "move.to.generate.dismissed"
+        static let CONFIGURE_OCR = "ocr.configure.dismissed"
+        static let PRIVACY_OPENED = "privacy.opened"
+        static let PRIVACY_DISSMISED = "privacy.dissmissed"
+        static let REMINDER_DATE = "reminder.date"
+        static let REMINDER_INSERTS = "reminder.inserts"
+        static let REMINDER_HAS_BACKUP = "reminder.has.backup"
+        static let REMINDER_PLUS_BACKUP = "reminder.plus.backup"
+    }
+    
+    enum Constants {
+        static let MIN_INSERTS_COUNT = 15
+        static let MIN_DAYS_COUNT = 10
     }
 }
