f88bf245a73883d8bb1b0e86671c9af6e3a22a25
Bogdan Evsenev <evsenev.developer@gmail.com>
2020-05-06 04:10:03 +0300

    Added Payment Method Graphs

---
+++ SmartReceipts/Modules/Graphs/DataSets/GraphsPaymentMethodsDataSet.swift
@@ -0,0 +1,49 @@
+//
+//  GraphsPaymentMethodsDataSet.swift
+//  SmartReceipts
+//
+//  Created by Bogdan Evsenev on 06.05.2020.
+//  Copyright © 2020 Will Baumann. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+struct GraphsPaymentMethodDataSet: BarChartDatSetProtocol {
+    let data: [GraphsPaymentMethodData]
+    let xLabels: [String]
+    let entries: [BarChartDataEntry]
+    
+    var title: String {
+        return "Payment Methods"
+    }
+    
+    init(data: [GraphsPaymentMethodData], maxCount: Int = 5) {
+        self.data = data
+        
+        var filteredDataSets = data
+            .sorted(by: { $0.total.amount.doubleValue > $1.total.amount.doubleValue })
+            .filter { $0.total.amount.doubleValue != 0 }
+    
+        if filteredDataSets.count > maxCount {
+            filteredDataSets = Array(filteredDataSets[..<maxCount])
+        }
+        
+        self.entries = filteredDataSets
+            .enumerated()
+            .map { index, dataSet in
+                return BarChartDataEntry(x: Double(index), y: dataSet.total.amount.doubleValue)
+            }
+        
+        self.xLabels = filteredDataSets.reduce([String]()) { result, dataSet -> [String] in
+            return result.adding(dataSet.paymentMethod.presentedValue())
+        }
+    }
+}
+
+extension GraphsPaymentMethodDataSet {
+    struct GraphsPaymentMethodData {
+        let paymentMethod: PaymentMethod
+        let total: Price
+    }
+}
